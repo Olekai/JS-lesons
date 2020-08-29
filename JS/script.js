8b5ab4be3e62305e573d01162247c0edfceffc82
Olekai <kaidash001@gmail.com>
2020-08-29 14:01:31 +0200

    git init

---
+++ JS/script.js
@@ -0,0 +1,39 @@
+//"use strict";
+
+//alert("Hello");
+
+//const result = confirm("Are you here?");
+//console.log(result);
+
+//const answer = prompt("Вам есть 18?", "18");
+//console.log(answer);
+
+ //const answers =[];
+
+//answers[0] = prompt('what is your name?', "");
+//answers[1] = prompt('What is your familija?', "");
+//answers[2] = prompt('Ili mash lat?', "");
+
+//document.write(answers);
+//const category ='toys';
+
+//console.log(`https://someurl.com/${category}/5`);
+
+//const user= "Olek";
+
+//alert(`Hello,${user}`);
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
+
+const isChecked = true,
+      isClose = true;
+      
+console.log(isChecked && isClose);
+
