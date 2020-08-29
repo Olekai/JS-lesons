11e261e3bbc20e89b801ab6951fd09e3cacef475
Olekai <kaidash001@gmail.com>
2020-08-29 14:14:06 +0200

    "master"

--- JS/script.js
+++ JS/script.js
@@ -23,17 +23,17 @@
 
 //alert(`Hello,${user}`);
 
-let incr = 10,
-    decr = 10;
+//let incr = 10,
+   // decr = 10;
 
-incr++;
-decr--;
+//incr++;
+//decr--;
 
 console.log(incr);
 console.log(decr);
 
 const isChecked = true,
       isClose = true;
-      
+
 console.log(isChecked && isClose);
 
